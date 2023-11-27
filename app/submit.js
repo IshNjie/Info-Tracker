2259195754e4ae21df0c1c6654a20460af5a15ed
IshNjie <ishmaelnjie@hotmail.com>
2023-11-27 21:45:10 +0000

    Added comments

--- app/submit.js
+++ app/submit.js
@@ -1,21 +1,26 @@
 
-
+// Assign info container to append innerHTML later
 const info_container = document.getElementById('info_container')
 
-
+// Function to suibmit form 
 function formSubmit() {
 
+    // Create an object of inputs
     const obj = {}
+
+    // ID set in HTML file
     const fName = document.getElementById('FirstName').value;
     const lName = document.getElementById('LastName').value;
     const job = document.getElementById('Job').value;
     const skills = document.getElementById('Skills').value;
 
+    // Remove the input value when next user wants to input
     document.getElementById('FirstName').value = '';
     document.getElementById('LastName').value = '';
     document.getElementById('Job').value = '';
     document.getElementById('Skills').value = '';
 
+    // Create object with the entries above
     const entry = {
         FirstName : fName,
         LastName: lName,
@@ -23,17 +28,15 @@ function formSubmit() {
         skills: skills.split(',')
     }
 
-    const newObj = Object.assign(obj,entry)
-
-    console.log(fName)
-    console.log(lName)
+    // Create a new object to append to empty object
+    const newObj = Object.assign(obj,entry);
     
     infoCard(newObj);
-    console.log(newObj)
-
+    console.log(newObj);
 
 }
 
+// Create Info Card
 
 function infoCard(person){
 
@@ -41,11 +44,13 @@ function infoCard(person){
     // add a class to element
     infoEl.classList.add('info');
 
+    // Grab entries from the person object after submit
     const fName = person.FirstName;
     const lName = person.LastName;
     const job = person.Job;
     const skills = person.skills;
 
+    // Create InnerHTML to display information 
     const infoInnerHTML = `
     
     <div class = "info">
